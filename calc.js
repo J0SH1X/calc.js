9d446df8311bdc059fc36f114b9a432c00d06044
J0SH1X <aljoshua.hell@gmail.com>
2025-01-30 17:08:56 +0100

    calc: calc.js: send history to backend

--- calc.js
+++ calc.js
@@ -1,17 +1,27 @@
 $(document).ready(function() {
-    $('.keys button').click(function() {
-      var buttonText = $(this).text();
-      var dsiplay = $('#display');
-  
-      if (buttonText === '=') {
-        try {
-          var ergebnis = eval(dsiplay.val());
-          dsiplay.val(ergebnis);
-        } catch (error) {
-          dsiplay.val('Error');
-        }
+  function generateId() {
+      return 'id-' + Math.random().toString(36).substr(2, 16);
+  }
+
+  var browser_id = localStorage.getItem('browser_id') || (localStorage.setItem('browser_id', generateId()), localStorage.getItem('browser_id'));
+
+  $('.keys button').click(function() {
+      var btn = $(this).text()
+      var display = $('#display');
+      if (!display.length) return;
+
+      if (btn === '=') {
+          try {
+              var exp = display.val().trim();
+              if (!/^[\d+\-*/(). ]+$/.test(exp)) return display.val('Error'); //ensure no sql injection can be performned by sanitazing the input
+              var res = eval(exp);
+              display.val(res);
+              $.post('backend.php', { browser_id, expression: exp, result: res });
+          } catch {
+              display.val('Error');
+          }
       } else {
-        dsiplay.val(dsiplay.val() + buttonText);
+          display.val(display.val() + btn);
       }
-    });
   });
+});
